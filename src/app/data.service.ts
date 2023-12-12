26e89eaecca77959d1f682034a5d360551343275
PraveenKumarChettibilli <pchettib@uncc.edu>
2023-12-11 20:18:22 -0500

    updating backend

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -49,7 +49,7 @@ export class DataService {
         const token = localStorage.getItem('accessToken');
         const body=JSON.stringify(username);
         const headers = {'content-type': 'application/json','Authorization' : `Bearer ${token}`};
-        this.DataObservable = this.http.get('https://personal-budget-backend-final-2eb033e7b9f5.herokuapp.com//budget',{ headers: headers,params:{userid : username }}).pipe(shareReplay());
+        this.DataObservable = this.http.get('https://personal-budget-backend-final-2eb033e7b9f5.herokuapp.com/budget',{ headers: headers,params:{userid : username }}).pipe(shareReplay());
         //this.DataObservable = this.http.get('http://localhost:3000/budget',{ headers: headers }).pipe(shareReplay());
         return this.DataObservable;
     }
@@ -59,7 +59,7 @@ export class DataService {
       const headers = {'content-type': 'application/json','Authorization' : `Bearer ${token}`};
       const body=JSON.stringify(data);
       console.log(body)
-      return this.http.post('https://personal-budget-backend-final-2eb033e7b9f5.herokuapp.com//budget',body,{'headers':headers});
+      return this.http.post('https://personal-budget-backend-final-2eb033e7b9f5.herokuapp.com/budget',body,{'headers':headers});
       //return this.http.post('http://localhost:3000/budget',body,{'headers':headers});
     }
 
@@ -68,7 +68,7 @@ export class DataService {
       const headers = {'content-type': 'application/json','Authorization' : `Bearer ${token}`};
       const body=JSON.stringify(data);
       console.log(body)
-      return this.http.post('https://personal-budget-backend-final-2eb033e7b9f5.herokuapp.com//feedback',body,{'headers':headers});
+      return this.http.post('https://personal-budget-backend-final-2eb033e7b9f5.herokuapp.com/feedback',body,{'headers':headers});
       //return this.http.post('http://localhost:3000/feedback',body,{'headers':headers});
     }
 
@@ -76,7 +76,7 @@ export class DataService {
       const headers = {'content-type': 'application/json'};
       const body=JSON.stringify(data);
       console.log(body)
-      return this.http.post('https://personal-budget-backend-final-2eb033e7b9f5.herokuapp.com//users',body,{'headers':headers});
+      return this.http.post('https://personal-budget-backend-final-2eb033e7b9f5.herokuapp.com/users',body,{'headers':headers});
     }
 
     invaliduser(){
@@ -88,7 +88,7 @@ export class DataService {
       const body=JSON.stringify(data);
       console.log(body)
       // return this.http.post('https://personal-budget-backend-final-2eb033e7b9f5.herokuapp.com//auth',body,{'headers':headers}).subscribe((res:any)=>{
-        return this.http.post('https://personal-budget-backend-final-2eb033e7b9f5.herokuapp.com//auth/',body,{'headers':headers}).subscribe((res:any)=>{
+        return this.http.post('https://personal-budget-backend-final-2eb033e7b9f5.herokuapp.com/auth/',body,{'headers':headers}).subscribe((res:any)=>{
         console.log(res);
         this.userRecord['username'] = data.username;
         this.userRecord['password'] = data.password;
